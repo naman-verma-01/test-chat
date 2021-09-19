47aad1090cb20c818b83b4f5b5a5d8ce3116d261
Naman Verma <81072434+naman-verma-01@users.noreply.github.com>
2021-09-19 16:55:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Heroku Chatting app
+A basic Heroku/Flask Chat App 
+
+https://basic-chatting.herokuapp.com/
+
+
+(UNDER-DEVELOPMENT)
---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, make_response, redirect, request, session
+from flask_socketio import SocketIO, send, emit
+import os
+from flask_session import Session
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+	
+@app.route("/")
+def index():
+	if not session.get("name"):
+		return redirect("/login")
+	
+	return render_template('index.html')
+
+	
+@app.route("/login", methods=["POST", "GET"])
+def login():
+	if request.method == "POST":
+		session["name"] = request.form.get("name")
+		return redirect("/")
+	return render_template("login.html")
+	
+	
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",session.get("name") + " : " + data,broadcast=True)
+    
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host='127.0.0.1', port=5004)
